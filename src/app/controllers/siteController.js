38ea50bce546d006dea9c0f67f0637eea882c64f
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-04-06 15:49:27 +0700

    mongodb mongoose

--- src/app/controllers/siteController.js
+++ src/app/controllers/siteController.js
@@ -1,7 +1,16 @@
+const Course = require('../models/Course');
+
 class SiteController {
   // [GET] /news
-  index(req, res) {
-    res.render('home');
+  index(req, res, next) {
+    Course.find({})
+      .lean()
+      .then((courses) => {
+        res.render('home', {
+          courses,
+        });
+      })
+      .catch((error) => next(error));
   }
   // [GET] /search
   search(req, res) {
