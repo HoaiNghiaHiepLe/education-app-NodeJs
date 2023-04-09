a256892a4be11b82a1e37aaf29697853cc1eb7c0
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-04-09 18:22:36 +0700

    delete course

--- src/app/controllers/courseController.js
+++ src/app/controllers/courseController.js
@@ -5,7 +5,7 @@ class CourseController {
   show(req, res, next) {
     Course.findOne({ slug: req.params.slug })
       .lean()
-      .then((course) => {
+      .then(course => {
         res.render('courses/show', {
           course,
         });
@@ -16,16 +16,38 @@ class CourseController {
   create(req, res, next) {
     res.render('courses/create');
   }
+  // [GET] /courses/edit
+  edit(req, res, next) {
+    Course.findById(req.params.id)
+      .lean()
+      .then(course =>
+        res.render('courses/edit', {
+          course,
+        }),
+      )
+      .catch(next);
+  }
+  // [PUT] /courses/store
+  update(req, res, next) {
+    Course.updateOne({ _id: req.params.id }, req.body)
+      .then(() => res.redirect('/me/stored/courses'))
+      .catch(next);
+  }
   // [POST] /courses/store
   store(req, res, next) {
-    // res.json(req.body);
     const formData = req.body;
     formData.image = `https://img.youtube.com/vi/${req.body.videoId}/sddefault.jpg`;
     const course = new Course(formData);
     course
       .save()
       .then(() => res.redirect('/'))
-      .catch((error) => {});
+      .catch(error => {});
+  }
+  // [POST] /courses/:id
+  destroy(req, res, next) {
+    Course.deleteOne({ _id: req.params.id })
+      .then(() => res.redirect('back'))
+      .catch(next);
   }
 }
 
