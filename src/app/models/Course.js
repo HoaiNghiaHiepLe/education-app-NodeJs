19a4b98c8b25a305bd9e258c29ecc43c7c66cb67
HoaiNghiaHiepLe <hiep.lhn@gmail.com>
2023-04-10 23:37:36 +0700

    softDelete

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const slug = require('mongoose-slug-updater');
-mongoose.plugin(slug);
+const mongooseDelete = require('mongoose-delete');
 
 const Course = new Schema(
   {
@@ -16,5 +16,12 @@ const Course = new Schema(
     timestamps: true,
   },
 );
+
+//add plugin
+mongoose.plugin(slug);
+Course.plugin(mongooseDelete, {
+  deleteAt: true,
+  overrideMethods: 'all',
+});
 
 module.exports = mongoose.model('Course', Course);
